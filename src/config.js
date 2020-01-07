ae0e7841e6921529bad11e5e599d755cb7deb63b
nphiukhao <nphiukhao@gmail.com>
2020-01-06 17:46:26 -0800

    config commented out

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_BASE ||'https://spaced-rep-api.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
 }
+
+
+//process.env.REACT_APP_API_BASE ||'https://spaced-rep-api.herokuapp.com/api'
