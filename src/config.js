38a2a3146b4f06ec38dd465394177113db0a4b8e
nphiukhao <nphiukhao@gmail.com>
2020-01-08 20:54:04 -0800

    new api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://immense-island-55994.herokuapp.com/api',
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
 }
 
+//https://immense-island-55994.herokuapp.com/
 
+//'http://localhost:8000/api'
 //process.env.REACT_APP_API_BASE ||'https://spaced-rep-api.herokuapp.com/api'
