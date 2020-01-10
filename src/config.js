fc4982d1459e5e9bf9329737e3bc194de1f8d13b
nphiukhao <nphiukhao@gmail.com>
2020-01-09 16:35:33 -0800

    config updated with new heroku and package.json with deployment scripts

--- src/config.js
+++ src/config.js
@@ -3,7 +3,5 @@ export default {
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
 }
 
-//https://immense-island-55994.herokuapp.com/
-
 //'http://localhost:8000/api'
 //process.env.REACT_APP_API_BASE ||'https://spaced-rep-api.herokuapp.com/api'
